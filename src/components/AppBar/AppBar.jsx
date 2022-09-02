27657999e3f0e9884d760ebea348962e86cc7346
ed_bolma <ed.bolma21@gmail.com>
2022-09-03 01:22:16 +0200

    update

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,6 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-
 import Button from '@mui/material/Button';
 
 import { NavLink } from 'react-router-dom';
